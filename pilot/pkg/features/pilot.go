b6835353f5035e3623e16a4dd1a70b55adfc4f3e
Pulak Kumar Das <pulak.das@imesh.ai>
2026-01-15 14:04:13 +0530

    Add ConnectOriginate keepalive options

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -326,6 +326,24 @@ var (
 
 	DisableTrackRemainingMetrics = env.Register("DISABLE_TRACK_REMAINING_CB_METRICS", true,
 		"If disabled, the remaining metrics for circuit breakers will not be tracked.").Get()
+
+	ConnectOriginateKeepaliveProbes = env.Register(
+		"PILOT_CONNECT_ORIGINATE_KEEPALIVE_PROBES",
+		0,
+		"Maximum number of keepalive probes sent to ztunnel before deciding the connection is dead. "+
+			"Default is 0 (disabled). Linux defaults to 9. ").Get()
+
+	ConnectOriginateKeepaliveTime = env.Register(
+		"PILOT_CONNECT_ORIGINATE_KEEPALIVE_TIME",
+		0,
+		"The idle time (in seconds) after which keepalive probes start being sent to ztunnel. "+
+			"Default is 0 (disabled). Linux defaults to 7200s (2 hours). ").Get()
+
+	ConnectOriginateKeepaliveInterval = env.Register(
+		"PILOT_CONNECT_ORIGINATE_KEEPALIVE_INTERVAL",
+		0,
+		"The number of seconds between keepalive probes sent to ztunnel. "+
+			"Default is 0 (disabled). Linux defaults to 75s. ").Get()
 )
 
 // UnsafeFeaturesEnabled returns true if any unsafe features are enabled.
